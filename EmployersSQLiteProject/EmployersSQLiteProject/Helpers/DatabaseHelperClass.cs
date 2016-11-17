4e40729955cfc10501fe21c25a85d1f680dfd683
GARY MC HUGH <G00308668@gmit.ie>
2016-11-17 15:36:29 +0000

    Creating the dbHelper class

--- EmployersSQLiteProject/EmployersSQLiteProject/Helpers/DatabaseHelperClass.cs
+++ EmployersSQLiteProject/EmployersSQLiteProject/Helpers/DatabaseHelperClass.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EmployersSQLiteProject.Model;
+using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,29 @@ using System.Threading.Tasks;
 
 namespace EmployersSQLiteProject.Helpers
 {
-    class DatabaseHelperClass
+    public class DatabaseHelperClass
     {
-    }
+
+        SQLiteConnection dbConn;
+
+        //Create Table 
+        public async Task<bool> onCreate(string DB_PATH)
+        {
+            try
+            {
+                if (!CheckFileExists(DB_PATH).Result)
+                {
+                    using (dbConn = new SQLiteConnection(DB_PATH))
+                    {
+                        dbConn.CreateTable<Employees>();
+                    }
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        } //onCreate
+    } //DatabaseHelperClass
 }
