867e1d261f828c20138ce8c355bba4803c620e81
GARY MC HUGH <G00308668@gmit.ie>
2016-11-17 15:15:13 +0000

    Added Views that will be used

---
+++ EmployersSQLiteProject/EmployersSQLiteProject/Views/AddEmployee.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace EmployersSQLiteProject.Views
+{
+    public partial class AddEmployee : ContentPage
+    {
+        public AddEmployee()
+        {
+            InitializeComponent();
+        }
+    }
+}
