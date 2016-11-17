0e037a20122ba68117e0ca686bb55bc46e3a9e3d
GARY MC HUGH <G00308668@gmit.ie>
2016-11-17 16:56:08 +0000

    commented the readEmployee method

--- EmployersSQLiteProject/EmployersSQLiteProject/Helpers/DatabaseHelperClass.cs
+++ EmployersSQLiteProject/EmployersSQLiteProject/Helpers/DatabaseHelperClass.cs
@@ -51,12 +51,17 @@ namespace EmployersSQLiteProject.Helpers
             }
         }
  
+        //get back the employee that ahs been selected from the db
+        //pass the method the employee id that the suer wants
         public Employees ReadEmployee(int empId)
         {
+            //get a connection to the database
             using (var dbConn = new SQLiteConnection(App.DB_PATH))
             {
+                //execute the select statement on the employee table where the id is equal to the empId selected
                 var existingEmployee = dbConn.Query<Employees>("select * from Employees where Id =" + empId).FirstOrDefault();
                 return existingEmployee;
+                //reurn the employee that was returned
             }
         }
     } //DatabaseHelperClass
