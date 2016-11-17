141a03f94c44c9f87404ce8d9cba7affb65fc085
GARY MC HUGH <G00308668@gmit.ie>
2016-11-17 15:23:41 +0000

    Updated the model

--- EmployersSQLiteProject/EmployersSQLiteProject/Model/Employees.cs
+++ EmployersSQLiteProject/EmployersSQLiteProject/Model/Employees.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace EmployersSQLiteProject.Model
 {
-    class Employees
+    public class Employees
     {
+        [SQLite.PrimaryKey, SQLite.AutoIncrement]
+        public int employeeId { get; set; }
+        public string empName { get; set; }
+        public int empAge { get; set; }
+        public string empPhoneNumber { get; set; }
+        public string empEmail { get; set; }
+        public decimal empSalary { get; set; }
+        public string CreationDate { get; set; }
+
+        public Employees()
+        {
+        }
+        public Employees(string name, int age, string phone_no, string email, decimal salary)
+        {
+            empName = name;
+            empAge = age;
+            empPhoneNumber = phone_no;
+            empEmail = email;
+            empSalary = salary;
+            CreationDate = DateTime.Now.ToString();
+        }
     }
 }
