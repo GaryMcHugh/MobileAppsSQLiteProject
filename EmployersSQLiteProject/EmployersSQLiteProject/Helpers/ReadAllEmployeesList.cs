813cbf880e0bb4aa285a6bc1a3fc8de4708ab637
GARY MC HUGH <G00308668@gmit.ie>
2016-11-19 18:42:43 +0000

    Added a read all emoloyees class

--- EmployersSQLiteProject/EmployersSQLiteProject/Helpers/ReadAllEmployeesList.cs
+++ EmployersSQLiteProject/EmployersSQLiteProject/Helpers/ReadAllEmployeesList.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using EmployersSQLiteProject.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EmployersSQLiteProject.Helpers
 {
-    class ReadAllEmployeesList
+    public class ReadAllEmployeesList
     {
+        DatabaseHelperClass Db_Helper = new DatabaseHelperClass();
+        public ObservableCollection<Employees> GetAllEmployees()
+        {
+            return Db_Helper.ReadEmployees();
+        }
     }
 }
