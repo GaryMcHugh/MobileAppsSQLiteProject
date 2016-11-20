e10c326f9c94f1c661dd861862b01fe074a91f2d
GARY MC HUGH <G00308668@gmit.ie>
2016-11-20 17:18:09 +0000

    Replaced incorrect views

--- EmployersSQLiteProject/EmployersSQLiteProject/Views/ReadEmployeesList.xaml.cs
+++ EmployersSQLiteProject/EmployersSQLiteProject/Views/ReadEmployeesList.xaml.cs
@@ -1,18 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
 
-using Xamarin.Forms;
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace EmployersSQLiteProject.Views
 {
-    public partial class ReadEmployeesList : ContentPage
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ReadEmployeesList : Page
     {
         public ReadEmployeesList()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
     }
 }
