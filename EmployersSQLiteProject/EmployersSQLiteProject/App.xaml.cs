43f76b05cd86faa8224a95588b20a56a2a582993
GARY MC HUGH <G00308668@gmit.ie>
2016-11-21 17:01:23 +0000

    Added delete all button

--- EmployersSQLiteProject/EmployersSQLiteProject/App.xaml.cs
+++ EmployersSQLiteProject/EmployersSQLiteProject/App.xaml.cs
@@ -114,7 +114,7 @@ namespace EmployersSQLiteProject
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(AddEmployee), e.Arguments);
+                    rootFrame.Navigate(typeof(ReadEmployeesList), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
